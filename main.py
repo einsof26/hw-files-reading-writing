1acdd8dc977d0670fcad510818b17c8500ea73d0
einsof26 <einsof26@gmail.com>
2022-06-16 07:42:56 +0300

    Task2

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 def book_cook_dict(file_name):
     with open(file_name, "rt",encoding="utf-8") as file:
+        global cook_book
         cook_book = {}
         for line in file:
             dish_name = file.readline().strip()
@@ -17,5 +18,23 @@ def book_cook_dict(file_name):
                 print(item)
             print()
 
-            
+
 book_cook_dict("catalog.txt")
+print("====================================================================")  
+
+def get_shop_list_by_dishes(dishes, person_count=1):
+    new_dict = {}
+    for i in range(len(dishes)):
+        x = dishes[i]   #Блюдо из списка заказа
+        y = cook_book[x] #Список словарей с ингридиентами заказанного блюда
+        total = {}
+        for elem in y:
+            new_dict[elem['ingredient_name']] = {'quantity': int(elem['quantity'])*person_count,'measure': elem['measure']}#Новый словарь с ключем - имя ингридиента
+                                                                                                         #и значением - количество и единица измерения
+            total.update(new_dict)
+    print("Необходимые ингридиенты для приготовления заказанных блюд: ")
+    for k,v in total.items():
+        print(f'{k} : {v}')
+        
+        
+get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
